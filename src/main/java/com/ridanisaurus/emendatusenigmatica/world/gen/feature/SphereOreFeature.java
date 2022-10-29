40dbbd3cfe7c7dc503ee1f2432586b105f51467c
Ridanis <khaled.chikhou@gmail.com>
2022-10-29 16:56:03 +0200

    fixed SphereOreFeature

--- src/main/java/com/ridanisaurus/emendatusenigmatica/world/gen/feature/SphereOreFeature.java
+++ src/main/java/com/ridanisaurus/emendatusenigmatica/world/gen/feature/SphereOreFeature.java
@@ -10,6 +10,7 @@ import com.ridanisaurus.emendatusenigmatica.loader.deposit.model.common.CommonBl
 import com.ridanisaurus.emendatusenigmatica.loader.deposit.model.sphere.SphereDepositModel;
 import com.ridanisaurus.emendatusenigmatica.loader.parser.model.StrataModel;
 import com.ridanisaurus.emendatusenigmatica.registries.EERegistrar;
+import com.ridanisaurus.emendatusenigmatica.registries.EETags;
 import com.ridanisaurus.emendatusenigmatica.util.MathHelper;
 import com.ridanisaurus.emendatusenigmatica.util.WorldGenHelper;
 import com.ridanisaurus.emendatusenigmatica.world.gen.feature.config.SphereOreFeatureConfig;
@@ -24,6 +25,7 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITag;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -43,12 +45,10 @@ public class SphereOreFeature extends Feature<SphereOreFeatureConfig> {
     }
 
     @Override
-    public boolean place(FeaturePlaceContext<SphereOreFeatureConfig> p_159749_) {
-        return false;
-    }
-
-    @Override
-    public boolean place(SphereOreFeatureConfig config, WorldGenLevel reader, ChunkGenerator generator, Random rand, BlockPos pos) {
+    public boolean place(FeaturePlaceContext<SphereOreFeatureConfig> config) {
+        Random rand = config.random();
+        BlockPos pos = config.origin();
+        WorldGenLevel reader = config.level();
         if (!model.getDimensions().contains(WorldGenHelper.getDimensionAsString(reader.getLevel()))) {
             return false;
         }
@@ -106,23 +106,23 @@ public class SphereOreFeature extends Feature<SphereOreFeatureConfig> {
                     if (y + yPos > yTop || y + yPos < yBottom) {
                         continue;
                     }
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ x, yPos + y, pos.getZ() + z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ -x, yPos + y, pos.getZ() + z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ x, yPos + -y, pos.getZ() + z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ x, yPos + y, pos.getZ() + -z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ -x, yPos + -y, pos.getZ() + z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ x, yPos + -y, pos.getZ() + -z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ -x, yPos + y, pos.getZ() + -z), config);
-                    placeBlock(reader, generator, rand, new BlockPos(pos.getX()+ -x, yPos + -y, pos.getZ() + -z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ x, yPos + y, pos.getZ() + z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ -x, yPos + y, pos.getZ() + z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ x, yPos + -y, pos.getZ() + z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ x, yPos + y, pos.getZ() + -z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ -x, yPos + -y, pos.getZ() + z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ x, yPos + -y, pos.getZ() + -z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ -x, yPos + y, pos.getZ() + -z), config);
+                    placeBlock(reader, rand, new BlockPos(pos.getX()+ -x, yPos + -y, pos.getZ() + -z), config);
                 }
             }
         }
         return true;
     }
 
-    private void placeBlock(WorldGenLevel reader, ChunkGenerator generator, Random rand, BlockPos
-            pos, SphereOreFeatureConfig config) {
-        if (!config.target.test(reader.getBlockState(pos), rand)) {
+    private void placeBlock(WorldGenLevel reader, Random rand, BlockPos
+            pos, FeaturePlaceContext<SphereOreFeatureConfig> config) {
+        if (!config.config().target.test(reader.getBlockState(pos), rand)) {
             return;
         }
         if (rand.nextInt(100) > model.getConfig().getDensity()) {
@@ -136,9 +136,10 @@ public class SphereOreFeature extends Feature<SphereOreFeatureConfig> {
                 Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(commonBlockDefinitionModel.getBlock()));
                 reader.setBlock(pos, block.defaultBlockState(), 2);
             } else if (commonBlockDefinitionModel.getTag() != null) {
-                ITag<Block> blockITag = BlockTags.getAllTags().getTag(new ResourceLocation(commonBlockDefinitionModel.getTag()));
-                Block block = blockITag.getRandomElement(rand);
-                reader.setBlock(pos, block.defaultBlockState(), 2);
+                ITag<Block> blockITag = ForgeRegistries.BLOCKS.tags().getTag(EETags.getBlockTag(new ResourceLocation(commonBlockDefinitionModel.getTag())));
+                blockITag.getRandomElement(rand).ifPresent(block -> {
+                    reader.setBlock(pos, block.defaultBlockState(), 2);
+                });
             } else if (commonBlockDefinitionModel.getMaterial() != null) {
                 BlockState currentFiller = reader.getBlockState(pos);
                 String fillerId = currentFiller.getBlock().getRegistryName().toString();
